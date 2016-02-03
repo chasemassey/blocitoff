6a10cc71f820e7aecfff2885ed96c66ffa1e9eb7
CharlesMassey <chasemassey@gmail.com>
2016-02-03 18:58:00 -0500

    Installed and configured Devise. Did not turn on email confirmation.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,6 +36,9 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smpt
+  config.action_mailer.perform_deliveries = true
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
