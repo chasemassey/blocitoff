f47d12ed5a5f7d8f34cd6ea60016a63268533cb0
CharlesMassey <chasemassey@gmail.com>
2016-02-10 18:50:43 -0500

    Added User and Item seed data via the Faker gem.

--- Gemfile
+++ Gemfile
@@ -27,6 +27,8 @@ gem 'bootstrap-sass'
 
 gem 'devise'
 
+gem 'faker'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+# Create Users
+5.times do
+  user = User.new(
+    email:    Faker::Internet.email,
+    password: Faker::Lorem.characters(10)
+  )
+  user.save!
+end
+users = User.all
+
+# Create test member
+  member = User.new(
+    email: "test@example.com",
+    password: "password"
+  )
+  member.save!
+
+# Create Items
+  10.times do
+    Item.create!(
+      user: users.sample,
+      name: Faker::Lorem.sentence
+    )
+  end
+
+ puts "Seed finished"
+ puts "#{User.count} users created"
+ puts "#{Item.count} items created"
