5032d96ea3cbc34d1f0c004d62f6b09c09cc7983
CharlesMassey <chasemassey@gmail.com>
2016-02-18 00:12:13 -0500

    Finished user story 7 and 8. Fixed ajax delete, added table, and days till deleted.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
     resources :users, only: [:update, :show] do
-      resources :items, only: [:create, :new, :destroy]
+      resources :items, only: [:create, :destroy]
     end
 
     get 'welcome/index'
